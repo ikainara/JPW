678e3ed45b4b164b011927a56a2fac208d3f54a5
igor.kainara <igor.kainara@gmail.com>
2024-12-10 15:37:47 +0100

    formatting fixes

--- src/main/java/parameterResolvers/InsertExtension.java
+++ src/main/java/parameterResolvers/InsertExtension.java
@@ -5,26 +5,24 @@ import com.microsoft.playwright.Page;
 import org.junit.jupiter.api.extension.BeforeEachCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 
-import java.lang.reflect.Constructor;
 import java.util.Arrays;
 
 public class InsertExtension implements BeforeEachCallback {
     @Override
-    public void beforeEach(ExtensionContext context) throws Exception {
+    public void beforeEach(ExtensionContext context) {
         var testInstance = context.getRequiredTestInstance();
         var fields = context.getRequiredTestClass().getDeclaredFields();
         var annotatedFields = Arrays.stream(fields).filter(f -> f.isAnnotationPresent(Insert.class)).toList();
         for (var field : annotatedFields) {
-            Constructor constructor;
             try {
-                constructor = field.getType().getConstructor(Page.class);
+                var constructor = field.getType().getConstructor(Page.class);
+                field.setAccessible(true);
+                var page = PwPageParameterResolver.getPage(context);
+                field.set(testInstance, constructor.newInstance(page));
             } catch (Exception e) {
                 throw new RuntimeException("Field '" + field.getName() + "' annotated with Insert annotation, but it class "
                         + field.getType().getTypeName() + "has no public constructor with Page.class parameter");
             }
-            field.setAccessible(true);
-            var page = PwPageParameterResolver.getPage(context);
-            field.set(testInstance, constructor.newInstance(page));
         }
     }
 }
